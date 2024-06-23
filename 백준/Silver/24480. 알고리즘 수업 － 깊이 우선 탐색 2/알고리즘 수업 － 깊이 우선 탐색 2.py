3b62597bca1e894844cfd5912bd4e18ee61ef501
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-06-23 17:41:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1336 ms, Memory: 680384 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+def dfs(graph, v, visited, order, counter):
+    visited[v] = True
+    order[v] = counter[0]
+    counter[0] += 1
+    for i in sorted(graph[v], reverse=True):
+        if not visited[i]:
+            dfs(graph, i, visited, order, counter)
+
+V, E, R = map(int, input().split())
+graph = [[] for _ in range(V + 1)]
+visited = [False] * (V + 1)
+order = [0] * (V + 1)
+counter = [1]
+
+for _ in range(E):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+dfs(graph, R, visited, order, counter)
+
+for i in range(1, V + 1):
+    print(order[i])
