38a427d656bc9de709ce347463c2ab503cb1fac5
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-06-24 19:55:37 +0900

    [Silver III] Title: 바이러스, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,25 @@
-import sys
-from collections import deque
+def dfs(graph, v, visited, order, count) :
+    visited[v] = True
+    order[v] = count[0]
+    count[0] += 1
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited, order, count)
+
 
 N = int(input())
 K = int(input())
-result = 0
-graph = [[] for _ in range(N + 1)]
-visited = [0] * (N+1)
 
-for m in range(K):
-    a,b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = 1 
-    while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1) 
+order = [0] * (N+1)
+count = [1]
 
-bfs(graph, 1, visited)
-print(sum(visited)-1)        
+for _ in range(K) :
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+dfs(graph, 1, visited, order, count)
+
+print(max(order) - 1)
