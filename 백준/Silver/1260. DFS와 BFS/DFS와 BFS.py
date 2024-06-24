26f9f0539c405053b2c177a17173ff114a684330
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-06-24 20:03:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 192 ms, Memory: 115636 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,34 @@
-import sys
 from collections import deque
 
-N, M, start = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-DFS_visited = [False] * (N+1)
-BFS_visited = [False] * (N+1)
-
-for m in range(M):
-    a,b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-for i in range(1, N+1):
-    graph[i].sort()
-    
-def bfs(graph, start, visited): 
-    queue = deque([start])
-    visited[start] = True
-    while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
-                                
-def dfs(graph, v, visited):
+def dfs(graph, v, visited) :
     visited[v] = True
     print(v, end=' ')
-    for i in graph[v]:
+    for i in sorted(graph[v]):
         if not visited[i]:
             dfs(graph, i, visited)
 
-dfs(graph, start, DFS_visited)
+def bfs(graph, v, visited):
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in sorted(graph[v]):
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+        
+N, M, V = map(int, input().split()) 
+
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1) 
+
+for _ in range(M) :
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+dfs(graph, V, visited)
+visited = [False] * (N+1)
 print()
-bfs(graph, start, BFS_visited)
+bfs(graph, V, visited)
