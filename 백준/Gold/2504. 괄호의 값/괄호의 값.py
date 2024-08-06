77f1691260eb29a2308178c1d098735a84aeca8e
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-08-06 19:38:44 +0900

    [Gold V] Title: 괄호의 값, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,42 +1,92 @@
 import sys
-input = sys.stdin.readline
+L = list(sys.stdin.readline().rstrip())
+left_stack = []
+right_stack = []
+answer = []
+ans = 0
+flag = 0
+temp = 0
 
-def calculate_bracket_value(brackets):
-    stack = []
-    temp = 1
-    result = 0
-    
-    for i in range(len(brackets)):
-        char = brackets[i]
-        
-        if char == '(':
-            stack.append(char)
-            temp *= 2
-        
-        elif char == '[':
-            stack.append(char)
-            temp *= 3
-        
-        elif char == ')':
-            if not stack or stack[-1] != '(':
-                return 0
-            if brackets[i - 1] == '(':
-                result += temp
-            stack.pop()
-            temp //= 2
-        
+for char in L:
+
+    if char == '[' or char == '(':
+        if not left_stack:
+            temp = 0
+            left_stack.append(char)
+        else:
+            if right_stack:
+                outer = right_stack.pop()
+                left_stack.append(outer)
+                right_stack.append(char)
+            else:
+                right_stack.append(char)
+
+    else:
+        if char == ')':
+            if len(right_stack) != 0 and right_stack[-1]  != '(':
+                print(0)
+                flag = 1
+                break
+            elif len(right_stack) != 0 and right_stack[-1] == '(':
+                right_stack.pop()
+                ans += 2
+                if left_stack:
+                    temp = 1
+                    for chars in left_stack:
+                        if chars == '(':
+                            ans *= 2
+                        else:
+                            ans *= 3
+                answer.append(ans)
+                ans = 0
+                temp = 1
+                
+            elif len(left_stack) != 0 and left_stack[-1] == '(':
+                left_stack.pop()
+                if temp == 0:
+                    ans += 2
+                    answer.append(ans)
+                    ans = 0
+            else:
+                print(0)
+                flag = 1
+                break
+                
         elif char == ']':
-            if not stack or stack[-1] != '[':
-                return 0
-            if brackets[i - 1] == '[':
-                result += temp
-            stack.pop()
-            temp //= 3
+            if len(right_stack) != 0 and right_stack[-1]  != '[':
+                print(0)
+                flag = 1
+                break
+            elif len(right_stack) != 0 and right_stack[-1] == '[':
+                right_stack.pop()
+                ans += 3
+                if left_stack:
+                    temp = 1
+                    for chars in left_stack:
+                        if chars == '(':
+                            ans *= 2
+                        else:
+                            ans *= 3
+                answer.append(ans)
+                ans = 0
+                temp = 1
+                
+            elif len(left_stack) != 0 and left_stack[-1] == '[':
+                left_stack.pop()
+                if temp == 0:
+                    ans += 3
+                    answer.append(ans)
+                    ans = 0
+            else:
+                print(0)
+                flag = 1
+                break
+            
+    if not left_stack:
+        answer.append(ans)
+        ans = 0
     
-    if stack:
-        return 0
-    
-    return result
-
-brackets = input().strip()
-print(calculate_bracket_value(brackets))
+if len(left_stack) == 0 and len(right_stack) == 0 and flag == 0:
+    print(sum(answer))
+elif (len(left_stack) != 0 or len(right_stack) != 0) and flag == 0 :
+    print(0)
