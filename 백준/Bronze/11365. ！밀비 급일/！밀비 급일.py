5759aa73925125d40a3862662f09dfb7d9355e7d
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-08-19 14:53:17 +0900

    [Bronze IV] Title: !밀비 급일, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.py
@@ -0,0 +1,14 @@
+import sys
+flag = 1
+
+while True:
+    
+    S = list(sys.stdin.readline().rstrip())
+    ans = ""
+    
+    if S[0] == 'E' and S[1] == 'N' and S[2] == 'D':
+        sys.exit()
+    
+    for i in range(len(S)-1, -1, -1):
+        ans += S[i]
+    print(ans)
