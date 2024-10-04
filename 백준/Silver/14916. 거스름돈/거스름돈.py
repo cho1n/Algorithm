7ddfd4e29d938cfab690ea40d5bd1732383eba94
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-10-04 18:57:21 +0900

    [Silver V] Title: 거스름돈, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,30 @@
+n = int(input())
+count = 0
+flag = 1
+
+while True:
+    if n == 3 or n < 2 or n > 100000:
+        flag = 0
+        break
+    
+    if n % 5 == 0:
+        count += (n//5)
+        break
+    
+    if (n - 5) % 2 == 0 :
+        n -= 5
+        count += 1
+    elif n%2 == 0:
+        n -= 2
+        count += 1
+    else:
+        flag = 0
+        break
+
+    if n == 0:
+        break
+
+if flag:
+    print(count)
+else:
+    print(-1)
