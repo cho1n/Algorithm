b6fc678893b7da5acd83e217c0ad0b65984ce5c6
Choi Jae Won <112847766+cho1n@users.noreply.github.com>
2024-10-05 14:58:06 +0900

    [Silver III] Title: 블로그2, Time: 152 ms, Memory: 156568 KB -BaekjoonHub

---
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -0,0 +1,23 @@
+N = int(input())
+colors = list(map(str, input()))
+B_count = R_count = 0
+start_inx = colors[0]
+
+for l in range(1, N):
+    if start_inx == 'B' and start_inx != colors[l]:
+        B_count += 1
+        start_inx = 'R'
+    elif start_inx == 'R' and start_inx != colors[l]:
+        R_count += 1
+        start_inx = 'B'
+        
+if colors[-1] == 'B':
+    B_count += 1
+else:
+    R_count += 1
+            
+
+if B_count > R_count:
+    print(R_count + 1)
+else:
+    print(B_count + 1)
